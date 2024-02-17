5f37d6214a012c2e51feaefbd22864a4ca0bfc4d
aar1976871 <123432500+aar1976871@users.noreply.github.com>
2024-02-17 15:57:34 -0600

    Create Stoplight.java

---
+++ Assignment2/Stoplight.java
@@ -0,0 +1,29 @@
+package edu.niu.android.stoplight;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Stoplight 
+{
+
+    public Stoplight()
+    {
+
+    }
+
+    public boolean redLightOn(View textView) //returns true if red light is on
+    {
+        return false;
+    }
+
+    public boolean yellowLightOn(View textView) //returns true if yellow light is on
+    {
+        return false;
+    }
+
+    public boolean greenLightOn(View textView) //returns true if green light is on
+    {
+        return false;
+    }
+}
