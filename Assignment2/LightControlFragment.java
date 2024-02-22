f91dc821e742dbecfb4ea86e205748d71187331e
aar1976871 <123432500+aar1976871@users.noreply.github.com>
2024-02-22 17:19:31 -0600

    Added documentation

--- Assignment2/LightControlFragment.java
+++ Assignment2/LightControlFragment.java
@@ -1,3 +1,9 @@
+/************************************************
+ * Class Name: LightFragment.java               *
+ * Purpose: This class manages the fragment     *
+ *          that displays the button that       *
+ *          changes the stoplight               *
+ ************************************************/
 package edu.niu.android.stoplight;
 
 import android.app.Fragment;
@@ -16,6 +22,6 @@ public class LightControlFragment extends Fragment
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState)
     {
-        return inflater.inflate(R.layout.fragment_light_control, container, false);
+        return inflater.inflate(R.layout.fragment_light_control, container, false); //inflates the xml fragment
     }
 }
