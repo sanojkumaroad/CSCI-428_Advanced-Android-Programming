508bdac8fc9701446243b5d3989ab0d6b2b4d95f
aar1976871 <123432500+aar1976871@users.noreply.github.com>
2024-02-22 17:30:28 -0600

    Added documentation

--- Assignment2/Stoplight.java
+++ Assignment2/Stoplight.java
@@ -1,29 +1,62 @@
+ /************************************************
+ * Class Name: Stoplight.java                    *
+ * Purpose: This class has boolean methods to    *
+ *          determine which light is on          *
+ ************************************************/
 package edu.niu.android.stoplight;
 
-import android.app.Activity;
-import android.os.Bundle;
+import android.graphics.drawable.ColorDrawable;
 import android.view.View;
 
-public class Stoplight 
-{
+public class Stoplight {
 
     public Stoplight()
     {
-
     }
 
+    /*********************************************************************
+     * redLightOn, and the two methods for the other two lights          *
+     * are alike, with the exception being the color that the background *
+     * color of the view is being compared to (red,green,yellow)         *
+     ********************************************************************/
     public boolean redLightOn(View textView) //returns true if red light is on
     {
+        //the color of the view is found and placed in an int variable
+        ColorDrawable viewColor = (ColorDrawable) textView.getBackground();
+        int color = viewColor.getColor();
+
+        //this if-statement compares the color of the view and the color in colors.xml
+        if(color == textView.getResources().getColor(R.color.red))
+        {
+            return true;
+        }
+
         return false;
     }
 
     public boolean yellowLightOn(View textView) //returns true if yellow light is on
     {
+        ColorDrawable viewColor = (ColorDrawable) textView.getBackground();
+        int color = viewColor.getColor();
+
+        if(color == textView.getResources().getColor(R.color.yellow))
+        {
+            return true;
+        }
+
         return false;
     }
 
     public boolean greenLightOn(View textView) //returns true if green light is on
     {
+        ColorDrawable viewColor = (ColorDrawable) textView.getBackground();
+        int color = viewColor.getColor();
+
+        if(color == textView.getResources().getColor(R.color.green))
+        {
+            return true;
+        }
+
         return false;
     }
 }
